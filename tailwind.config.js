04d7c0c1ef7125e52d1ed225f4b610cf60056099
danialchoudary <danialchoudary255@gmail.com>
2024-10-26 09:44:37 +0500

    Your commit message describing the changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,16 +11,6 @@ export default {
 }
 
 
-// tailwind.config.js
-module.exports = {
-  theme: {
-    extend: {
-      animation: {
-        bounce200: 'bounce 0.75s infinite 0.2s',
-        bounce400: 'bounce 0.75s infinite 0.4s',
-      },
-    },
-  },
-};
+
 
 
